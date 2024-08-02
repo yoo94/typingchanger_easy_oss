8164e8d1af44ebe9e355d2920ae490065e519767
yoo94 <ahy0119@naver.com>
2024-08-02 21:40:10 +0900

    test: 올바른 영어 단어인지 구분하는 test 추가

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,10 @@
-const { convertKoreanToEnglish, convertEnglishToKorean, isCorrectKoreanString } = require('../typingChanger_easy');
+const { convertKoreanToEnglish, convertEnglishToKorean, isCorrectKoreanString, isValidEnglishWord } = require('../typingChanger_easy');
 
 describe('typingChanger_easy.js 테스트', () => {
-    test('isCorrectKoreanString: 올바른 한글 문자열', () => {
-        expect(isCorrectKoreanString('안녕하세요')).toBe(true);
+    beforeAll(async () => {
+        // 사전 데이터를 로드하기 위해 약간의 시간이 필요할 수 있습니다.
+        await new Promise(resolve => setTimeout(resolve, 1000));
     });
-
-    test('isCorrectKoreanString: 잘못된 한글 문자열', () => {
-        expect(isCorrectKoreanString('ㅇㅏㄴ하')).toBe(false);
-    });
-
     test('convertKoreanToEnglish: 한글을 영어로 변환', () => {
         const input = '안녕하세요';
         const output = convertKoreanToEnglish(input);
@@ -19,5 +15,20 @@ describe('typingChanger_easy.js 테스트', () => {
         const input = 'dkssudgktpdy';
         const output = convertEnglishToKorean(input);
         expect(output).toBe('안녕하세요');
+    });
+
+    test('isCorrectKoreanString: 올바른 한글 문자열', () => {
+        expect(isCorrectKoreanString('안녕하세요')).toBe(true);
+    });
+
+    test('isCorrectKoreanString: 잘못된 한글 문자열', () => {
+        expect(isCorrectKoreanString('ㅇㅏㄴ하')).toBe(false);
+    });
+    test('isValidEnglishWord: 올바른 영어 단어', () => {
+        expect(isValidEnglishWord('campus')).toBe(true);
+    });
+
+    test('isValidEnglishWord: 잘못된 영어 단어', () => {
+        expect(isValidEnglishWord('facamp')).toBe(false);
     });
 });
