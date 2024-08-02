9f25a8dc2d037b850474b8c196869ece1667b4a9
yoo94 <ahy0119@naver.com>
2024-08-02 21:51:00 +0900

    test: 변환 및 검증 테스트 코드 추가

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { convertKoreanToEnglish, convertEnglishToKorean, isCorrectKoreanString, isValidEnglishWord } = require('../typingChanger_easy');
+const { convertKoreanToEnglish, convertEnglishToKorean, isCorrectKoreanString, isValidEnglishWord, processText } = require('../typingChanger_easy');
 
 describe('typingChanger_easy.js 테스트', () => {
     beforeAll(async () => {
@@ -24,6 +24,7 @@ describe('typingChanger_easy.js 테스트', () => {
     test('isCorrectKoreanString: 잘못된 한글 문자열', () => {
         expect(isCorrectKoreanString('ㅇㅏㄴ하')).toBe(false);
     });
+
     test('isValidEnglishWord: 올바른 영어 단어', () => {
         expect(isValidEnglishWord('campus')).toBe(true);
     });
@@ -31,4 +32,17 @@ describe('typingChanger_easy.js 테스트', () => {
     test('isValidEnglishWord: 잘못된 영어 단어', () => {
         expect(isValidEnglishWord('facamp')).toBe(false);
     });
+
+    test('processText: 한글 텍스트 변환', () => {
+        const input = 'dkssudgktpdy';
+        const output = processText(input);
+        expect(output).toBe('안녕하세요');
+    });
+
+    test('processText: 영어 텍스트 변환', () => {
+        const input = 'ㄻㄴㅅ';
+        const output = processText(input);
+        expect(output).toBe('fast');
+    });
+
 });
