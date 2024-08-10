6f943a7327cdd38765875b890359906a51fbd029
yoo94 <ahy0119@naver.com>
2024-08-10 10:39:18 +0900

    refactor: JavaScript 파일을 TypeScript로 변환
    - 관련된 모든 파일의 확장자를 .js에서 .ts로 변경
    - tsconfig.json 설정 추가

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+const initialConsonants = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
+const vowels = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"];
+const finalConsonants = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
+
+const koreanToEnglishMap: { [key: string]: string } = {
+    'ㄱ': 'r', 'ㄲ': 'R', 'ㄴ': 's', 'ㄷ': 'e', 'ㄸ': 'E', 'ㄹ': 'f', 'ㅁ': 'a', 'ㅂ': 'q', 'ㅃ': 'Q', 'ㅅ': 't', 'ㅆ': 'T',
+    'ㅇ': 'd', 'ㅈ': 'w', 'ㅉ': 'W', 'ㅊ': 'c', 'ㅋ': 'z', 'ㅌ': 'x', 'ㅍ': 'v', 'ㅎ': 'g',
+    'ㅏ': 'k', 'ㅐ': 'o', 'ㅑ': 'i', 'ㅒ': 'O', 'ㅓ': 'j', 'ㅔ': 'p', 'ㅕ': 'u', 'ㅖ': 'P', 'ㅗ': 'h', 'ㅘ': 'hk', 'ㅙ': 'ho', 'ㅚ': 'hl',
+    'ㅛ': 'y', 'ㅜ': 'n', 'ㅝ': 'nj', 'ㅞ': 'np', 'ㅟ': 'nl', 'ㅠ': 'b', 'ㅡ': 'm', 'ㅢ': 'ml', 'ㅣ': 'l',
+    'ㄳ': 'rt', 'ㄵ': 'sw', 'ㄶ': 'sg', 'ㄺ': 'fr', 'ㄻ': 'fa', 'ㄼ': 'fq', 'ㄽ': 'ft',
+    'ㄾ': 'fx', 'ㄿ': 'fv', 'ㅀ': 'fg', 'ㅄ': 'qt'
+};
+
+const englishToKoreanMap: { [key: string]: string } = {};
+for (const koreanChar in koreanToEnglishMap) {
+    const englishChar = koreanToEnglishMap[koreanChar];
+    englishToKoreanMap[englishChar] = koreanChar;
+}
+
+export { initialConsonants, vowels, finalConsonants, koreanToEnglishMap, englishToKoreanMap };
---
+++ src/utils.ts
@@ -0,0 +1,69 @@
+import { initialConsonants, vowels, finalConsonants } from './constants';
+
+// 특수문자, 숫자 제외
+function removeSpecialCharactersAndNumbers(str: string): string {
+    return str.replace(/[^a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣\s]/g, '');
+}
+
+// 한글자 -> 자모음 분리
+function splitKoreanChar(koreanChar: string): string[] {
+    const baseCode = koreanChar.charCodeAt(0) - 0xAC00;
+    const initialIndex = Math.floor(baseCode / 588);
+    const vowelIndex = Math.floor((baseCode % 588) / 28);
+    const finalIndex = baseCode % 28;
+    return [initialConsonants[initialIndex], vowels[vowelIndex], finalConsonants[finalIndex]];
+}
+
+// 한글 문자열 자모 분리 함수
+function splitKoreanString(koreanString: string): string[] {
+    let result: string[] = [];
+    for (let i = 0; i < koreanString.length; i++) {
+        const char = koreanString[i];
+        if (char >= '가' && char <= '힣') {
+            result = result.concat(splitKoreanChar(char));
+        } else {
+            result.push(char);
+        }
+    }
+    return result;
+}
+
+// 자모 결합 함수
+function joinKoreanString(chars: string[]): string {
+    let result = '';
+    let i = 0;
+
+    while (i < chars.length) {
+        if (chars[i] === ' ') {
+            result += ' ';
+            i++;
+            continue;
+        }
+
+        const initial = chars[i++];
+        const vowel = chars[i++];
+        let final = '';
+
+        const hasFinalConsonant = finalConsonants.includes(chars[i]);
+        const hasNextVowel = i + 1 < chars.length && vowels.includes(chars[i + 1]);
+
+        if (i < chars.length && hasFinalConsonant && !hasNextVowel) {
+            final = chars[i++];
+        }
+
+        const initialIndex = initialConsonants.indexOf(initial);
+        const vowelIndex = vowels.indexOf(vowel);
+        const finalIndex = finalConsonants.indexOf(final);
+
+        if (initialIndex >= 0 && vowelIndex >= 0) {
+            const baseCode = 0xAC00 + (initialIndex * 588) + (vowelIndex * 28) + (finalIndex >= 0 ? finalIndex : 0);
+            result += String.fromCharCode(baseCode);
+        } else {
+            result += initial + vowel + final;
+        }
+    }
+
+    return result;
+}
+
+export { removeSpecialCharactersAndNumbers, splitKoreanString, joinKoreanString, splitKoreanChar };
